0afa3b0740152a90f1c3c8c302b7390c23b26035
aliraza <chaudaryaliraza0@gmail.com>
2024-11-12 19:45:36 +0500

    add frontend url to backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({ 
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://job-portal-client-gamma.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }))
